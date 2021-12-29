25e6ed4f70e8ff3a444c04c28aa84a5c8582f0d3
DarkBYT153#4092 <91132683+ShadowPlazs@users.noreply.github.com>
2021-12-29 22:36:57 +0800

    Create play.js

---
+++ main/play.js
@@ -0,0 +1,64 @@
+let RPS = 0
+let RPS_2 = 0
+input.onButtonPressed(Button.A, function () {
+    RPS = randint(1, 3)
+    if (RPS == 1) {
+        basic.showLeds(`
+            . # . . .
+            # # # # #
+            . # # . .
+            . # # # .
+            . . # # .
+            `)
+    } else if (RPS == 2) {
+        basic.showLeds(`
+            . . . . .
+            . # # # .
+            . # # # .
+            . # # # .
+            . . . . .
+            `)
+    } else if (RPS == 3) {
+        basic.showLeds(`
+            # # . . #
+            # # . # .
+            . . # . .
+            # # . # .
+            # # . . #
+            `)
+    } else {
+    	
+    }
+    basic.pause(1000)
+    RPS_2 = randint(1, 3)
+    if (RPS_2 == 1) {
+        basic.showLeds(`
+            . # . . .
+            # # # # #
+            . # # . .
+            . # # # .
+            . . # # .
+            `)
+    } else if (RPS_2 == 2) {
+        basic.showLeds(`
+            . . . . .
+            . # # # .
+            . # # # .
+            . # # # .
+            . . . . .
+            `)
+    } else if (RPS_2 == 3) {
+        basic.showLeds(`
+            # # . . #
+            # # . # .
+            . . # . .
+            # # . # .
+            # # . . #
+            `)
+    } else {
+    	
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    basic.clearScreen()
+})
